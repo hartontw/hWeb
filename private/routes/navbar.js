d6bd1ee916f9dd90e7dd7230a13e9cd2d06e074b
Harton <hartontw@gmail.com>
2019-02-13 02:31:39 +0100

    Added Projects

--- private/routes/navbar.js
+++ private/routes/navbar.js
@@ -2,6 +2,7 @@ const Buffer = require('buffer').Buffer;
 const express = require('express');
 const hbs = require(__dirname + '/../hbs');
 const navbar = require(__dirname + '/../config/navbar.json');
+const projects = require(__dirname + '/../config/projects.json');
 const mongoose = require('mongoose');
 
 const Article = require(process.env.ROOT + '/private/models/article');
@@ -42,6 +43,27 @@ const getNavbar = () => {
             app.get('/', (req, res) => {
                 res.render(hbs.getView(item.ref), params);
             });
+        }
+
+        if (item.ref === 'projects') {
+            let side = false;
+            projects.years.reverse();
+            projects.years.forEach((year) => {
+                year.months.reverse();
+                year.months.forEach((month) => {
+                    month.projects.reverse();
+                    month.projects.forEach((project) => {
+                        project.classA = side ? 'center-right' : 'center-left';
+                        project.classB = side ? 'offset-sm-6 col-sm-6' : 'col-sm-6';
+                        project.classC = side ? 'offset-sm-1 col-sm-11' : 'col-sm-11';
+                        project.classD = side ? 'debits anim animate fadeInRight' : 'credits anim animate fadeInLeft';
+                        side = !side;
+                    });
+                });
+            });
+
+            console.log(projects.years[0].months[0].projects[0].side);
+            params.years = projects.years;
         }
         app.get('/' + item.ref, (req, res) => {
             res.render(hbs.getView(item.ref), params);
